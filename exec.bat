39fa7eabadfd7a5ad99ea1d9c2dbf2ca3ca6fadf
jebusfx <aliens_exist41@hotmail.com>
2016-05-06 21:38:19 -0500

    csafasfas

---
+++ exec.bat
@@ -0,0 +1,2 @@
+python -W ignore src/alpsdocking.py files/cocaine.pdb files/transp.pdb files/transp_cavs.pdb files/cocaine.itp best.pdb
+::python -W ignore src/alpsdocking.py files/zinc_11909586.pdb files/1ubq.pdb files/cavities.pdb files/zinc_11909586.itp best.pdb
