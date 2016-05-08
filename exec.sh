9e8802c8db5a8bc45e57857976251b7e4277b5b6
despinosa <crypto.espinosa@gmail.com>
2016-05-08 13:58:20 -0500

    nuevo caso

--- exec.sh
+++ exec.sh
@@ -1,2 +1,7 @@
-python -W ignore src/alpsdocking.py files/cocaine.pdb files/transp.pdb files/transp_cavs.pdb files/cocaine.itp 2 ~/best.pdb
+hdid -nomount ram://256000  # 256000 (sector) * 512 (bytes/sector)
+newfs_hfs /dev/disk1
+mkdir /tmp/space
+mount -t hfs /dev/disk1 /tmp/space
+export TMPDIR=/tmp/ramdisk
+python -W ignore src/alpsdocking.py files/benzamidine.pdb files/trypsin.pdb files/trypsin_cavs.pdb files/benzamidine.itp 2 ~/best.pdb
 open ~/best.pdb
