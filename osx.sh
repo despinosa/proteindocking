fd9a4416ef3b8041cc015d5aacb7c50771fef6bf
despinosa <crypto.espinosa@gmail.com>
2016-05-19 13:37:37 -0500

    cosas para ejecutar

--- osx.sh
+++ osx.sh
@@ -1,10 +1,11 @@
-# hdid -nomount ram://2048000  # 256000 (sector) * 512 (bytes/sector)
-# newfs_hfs /dev/disk6
-# mkdir /tmp/ramdisk
-# mount -t hfs /dev/disk6 /tmp/ramdisk
-#
-# umount /tmp/ramdisk/
-# hdiutil detach /dev/disk6
-# export TMPDIR=/tmp/ramdisk
+if ["$(mount | grep /tmp/ramdisk)" = ""]; then
+    export RAMDEV=$(hdid -nomount ram://2048000)  # 256000 (sector) * 512 (bytes/sector)
+    newfs_hfs $RAMDEV
+    mkdir /tmp/ramdisk
+    mount -t hfs $RAMDEV /tmp/ramdisk
+fi
+export TMPDIR=/tmp/ramdisk
 python -W ignore src/alpsdocking.py files/tryp_benz/benzamidine.pdb files/tryp_benz/trypsin.pdb files/tryp_benz/trypsin_cavs.pdb files/tryp_benz/benzamidine.itp 1 .
-# open $TMPDIR/best.pdb
+open ./best*.pdb &
+umount /tmp/ramdisk/
+hdiutil detach $RAMDEV
